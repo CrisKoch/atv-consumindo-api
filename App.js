94e19ed296fab0603294ad91ab44e6719ac2911f
Cristina Kochmann <cristinak.iftm@gmail.com>
2021-09-26 18:26:39 -0300

    Correcao formatacao temperatura

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Text, View, SafeAreaView, ImageBackground, StyleSheet} from 'react-native';
 
+
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+ 
 
   const getDados = async () => {
     try {
@@ -29,7 +31,7 @@ export default App = () => {
             source={{ uri: 'https://blogdoenem.com.br/wp-content/uploads/2019/03/4-estacoes-do-ano.jpg' }}
             style={{ width: 400, height: 400 }}/>
           <Text style={styles.temp}>Uberlândia</Text>
-          <Text style={styles.umid}> {data.main.temp}ºC</Text>
+          <Text style={styles.umid}> {(data.main.temp).toFixed()}ºC</Text>
           <Text>Umidade {data.main.humidity}%</Text>
         </View>
       )}
@@ -42,6 +44,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+    
   },
   container2:{
     flex: 1, 
@@ -52,7 +55,8 @@ const styles = StyleSheet.create({
     padding: 10, 
     fontSize: 20, 
     color: 'red', 
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    
   },
   umid: {
     fontSize: 80
